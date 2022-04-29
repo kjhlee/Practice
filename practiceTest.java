180b152729b6dc3fc0877582b3638d47331800fe
theemotionalpie <52635340+theemotionalpie@users.noreply.github.com>
2022-04-29 16:54:34 -0700

    afdas

---
+++ practiceTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class practiceTest {
+    @Test
+    public void check()
+    {
+        assertEquals(false, practice.longWord("six"));
+    }
+}
